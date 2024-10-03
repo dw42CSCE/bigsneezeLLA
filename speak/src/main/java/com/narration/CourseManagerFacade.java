c5eb9c6b6d774708493d65d4e71c64c998fcf9f0
logan-df <ldford711@gmail.com>
2024-10-03 09:39:34 -0400

    CourseManagerFacade stub UserList stub

---
+++ speak/src/main/java/com/narration/CourseManagerFacade.java
@@ -0,0 +1,58 @@
+public class CourseManagerFacade {
+    private User user;
+    private Settings settings;
+    private UserList users;
+    private CourseList courses;
+
+    public CourseManagerFacade() {
+
+    }
+
+    public void login(String username, String password) {
+        
+    }
+
+    public User signUp(String username, String email, String password) {
+        return null;
+    }
+
+    public Course getCourse(string language) {
+        return null;
+    }
+
+    public CourseList getAllCourses() {
+        return null;
+    }
+
+    public UserList getUsers() {
+        return null;    
+    }
+
+    public void toggleDarkMode(boolean isDarkMode) {
+
+    }
+
+    public void toggleEmailNotification(boolean emailNotification) {
+
+    }
+
+    public Lesson getLesson(int index) {
+        return null;
+    }
+
+    public Exercise getExercise(int index) {
+        return null;
+    }
+
+    public boolean isCorrect(String userAnswer) {
+        return null;
+    }
+
+    public void logOut() {
+
+    }
+
+    public Settings getUserSettings() {
+        return null;
+    }
+}
