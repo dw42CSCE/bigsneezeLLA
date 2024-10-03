c1179c35926b45c43422a6e0ff65ba132add9a98
dw42CSCE <dw42@email.sc.edu>
2024-10-03 09:43:53 -0400

    Fixes #7, Fixes #5, Fixes #30, Fixes #29, Fixes #27

---
+++ speak/src/main/java/com/narration/Word.java
@@ -0,0 +1,26 @@
+package com.narration;
+
+public class Word {
+    private String word;
+    private String meaning;
+
+    public Word(String word, String meaning){
+        //ToDo
+    }
+
+    public String getWord(){
+        return "ToDo";
+    }
+
+    public String getMeaning(){
+        return "ToDo";
+    }
+
+    public void speak(){
+        //Uses Polly
+    }
+
+    public String toString(){
+        return "ToDo";
+    }
+}
