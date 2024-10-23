2a2d9f605d13293a610684978464d7eb38f859b8
KyleHix <khix@email.sc.edu>
2024-10-22 21:46:53 -0400

    updated UI, Scenario1

--- speak/src/main/java/com/narration/UserInterface.java
+++ speak/src/main/java/com/narration/UserInterface.java
@@ -3,67 +3,33 @@ import java.util.Scanner;
 import java.util.UUID;
 
 public class UserInterface {
-    
-    //Unfinished
-    public void Scenario1(){
-        String username="John";
-        String email="John@gmail.com";
-        String password="Password";
 
-         //New Facade
-         CourseManagerFacade facade=new CourseManagerFacade();
-         //Sign up
-         facade.signUp(username, email, password);
-         //Login
-         facade.login(username, password);
-        
-         //Set Course & Get course
-         //TODO 
+    private static  CourseManagerFacade facade;
+
+    public UserInterface(){
+        facade= new CourseManagerFacade();
+    }
+    
+    public static void run(){
+        Scenario1();
+    }
+    //Unfinished
+    public static void Scenario1(){
+
+         facade.signUp("pacman83", "pacman83@gmail.com", "123password");
+         facade.login("pacman83", "123password");
+
+         facade.getCourse(b1303bf7-6575-41ea-93fb-4bb8da658eda);
  
          //Pulling Lessons
          facade.setLesson(0);
          facade.getLesson();
+         facade.setExercise(0);
+         facade.getExercise();
 
          //Logout
          facade.logOut();
  
     }
-
-    public static void run(){
-        CourseManagerFacade facade=new CourseManagerFacade();
-        Scanner keyboard=new Scanner(System.in);
-        String userName=" ";
-        String password=" ";
-        String firstName=" ";
-        String lastName=" ";
-        String emailAddress=" ";
-
-        System.out.println("Enter 1 to sign up \nEnter 2 to login");
-        int signup=keyboard.nextInt();
-        if(signup==1){
-            userName.equals(keyboard.nextLine());
-            password.equals(keyboard.nextLine());
-            firstName.equals(keyboard.nextLine());
-            lastName.equals(keyboard.nextLine());
-            emailAddress.equals(keyboard.nextLine());
-            facade.signUp(userName, password, emailAddress);
-        }
-        else{
-            
-        }
-
-        
-
-
-    }
-   
-   
-   
-    public static void main(String[] args) {
-    
-    
-    }
-
-
 
 }
