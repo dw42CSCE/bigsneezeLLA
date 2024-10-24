07588b6b8bd9110a93aaa32980c7aac81ca224a6
logan-df <ldford711@gmail.com>
2024-10-24 11:59:57 -0400

    matching

--- speak/src/main/java/com/narration/Matching.java
+++ speak/src/main/java/com/narration/Matching.java
@@ -7,27 +7,31 @@ public class Matching extends Exercise{
     protected Word answer;
     protected ArrayList<Word> answers;
     private ArrayList<String> choices;
+    private int answerIndex;
 
     public Matching(ArrayList<Word> words){
         super(words);
         Random rand = new Random();
         this.answer = words.get(rand.nextInt(0, words.size()));
         choices = new ArrayList<String>(4);
-        int answerIndex = rand.nextInt(0, 4);
+        answerIndex = rand.nextInt(0, 4);
         for(int i = 0; i < choices.size(); i++) {
             if(i == answerIndex) {
                 choices.add(answer.getWord());
             }
-            // Randomly choose a similar word
+            choices.add(answer.getSimilarWords().get(i).getWord());
         }
     }
 
     public boolean isCorrect(String userAnswer){
-        return answer.getWord().equalsIgnoreCase(userAnswer.trim());
+        return answerIndex + 1 == Integer.parseInt(userAnswer);
     }
 
     public String toString(){
-        return "Which of the following words is the Spanish equivalent of " + answer + "?" +
-        "\n 1. ";
+        return "Which of the following words is the Spanish equivalent of " + answer.getMeaning() + "?" +
+        "\n 1. " + choices.get(0) +
+        "\n 2. " + choices.get(1) +
+        "\n 3. " + choices.get(2) +
+        "\n 4. " + choices.get(3);
     }
 }
