e0941a432369c7ec2e735bd1f13010776b029457
logan-df <ldford711@gmail.com>
2024-10-24 09:18:59 -0400

    nothing, really

--- speak/src/main/java/com/narration/Matching.java
+++ speak/src/main/java/com/narration/Matching.java
@@ -1,17 +1,20 @@
 package com.narration;
 
 public class Matching {
-    protected String questions;
+    protected String question;
     protected String options;
     protected Word answer;
     protected String type;
 
-    public Matching(String question, String answer){
-     
+    public Matching(String question, String options, String answer){
+        
     }
 
-    public boolean isCorrect(String username){
-        return true;
+    public boolean isCorrect(Word userAnswer){
+        if(answer.getWord().equalsIgnoreCase(userAnswer.getWord())) {
+            return true;
+        }
+        return false;
     }
 
     public String toString(){
