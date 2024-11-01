a7412a067ded65760081d933c5f099024373d032
KyleHix <khix@email.sc.edu>
2024-11-01 11:50:01 -0400

    WordListTest addWord tests complete

--- speak/src/test/java/com/narration/WordListTest.java
+++ speak/src/test/java/com/narration/WordListTest.java
@@ -2,21 +2,30 @@ package com.narration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Collectors;
+
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class WordListTest {
 
     private WordList words;
     private Word word;
     private Word testWord;
+
     @BeforeEach
     public void setUp(){
         words = new WordList();
         word = new Word("Test", "Test");
     }
     
+
+    //AddWord Tests
     @Test
     public void testAddWord(){
         this.words.addWord(word);
@@ -25,21 +34,39 @@ public class WordListTest {
     }
 
     @Test
-    public void testDuplicateWord(){
+    public void testAddDuplicateWord(){
         this.words.addWord(word);
         this.words.addWord("Test", "Test");
-        boolean duplicate=false;
-        for(int i=0, j=0; i<words.getWords().size();i++){
-            if(word.getWord().equals("Test"))
+        int j=0;
+        for(int i=0; i<words.getWords().size();i++){
+            if(word==words.getWord("Test")){
                 j++;
-                if(j==2)
-                    duplicate=true;
-                    break;
+            }
         }
-        assertTrue(duplicate);
+        assertEquals(1,j);
     }
-    
 
+    @Test
+    public void testNullWord(){
+        Word test = words.addWord(null, "Test");
+        assertNotNull(test.getWord());
+    }
+
+    @Test
+    public void testNullMeaning(){
+        Word test = words.addWord("Test", null);
+        assertNotNull(test.getMeaning());
+    }
+
+    @Test
+    public void testSameWordDiffMeaning(){
+        words.addWord(new Word("Test", "First Meaning"));
+        words.addWord(new Word("Test", "Different Meaning"));
+        assertEquals(words.getWords().get(0).getMeaning(), words.getWords().get(1).getMeaning());
+    }
+
+    
+    //Remove Word Tests
     @Test
     public void testRemoveWord(){
         words.addWord(word);
@@ -48,7 +75,7 @@ public class WordListTest {
         assertNotEquals(word, testWord);
     }
 
-    @Test
+ 
  
     
 
