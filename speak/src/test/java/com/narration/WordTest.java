06a09a36633761a9b027002ef6c9877e8e5d8bbb
Dallas Wade <143474044+dw42CSCE@users.noreply.github.com>
2024-11-01 20:47:45 -0400

    Update WordTest.java

--- speak/src/test/java/com/narration/WordTest.java
+++ speak/src/test/java/com/narration/WordTest.java
@@ -15,6 +15,16 @@ public class WordTest {
     }
 
     @Test
+    public void testNullWord(){
+        Word word1 = new Word(null, "Hello");
+        Word word2 = new Word("Hola", null);
+        assertEquals(word1.getMeaning(), "Hello");
+        assertEquals(word1.getWord(), "Default");
+        assertEquals(word2.getMeaning(), "Default");
+        assertEquals(word2.getWord(), "Hola");
+    }
+
+    @Test
     public void testToString(){
         Word word = new Word("Hola", "Hello");
         assertEquals(word.toString(), "Hola : Hello");
